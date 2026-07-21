4b8428a4cbcdafca771c2ec86a3c2d2ef31d1001
info4assistance25-maker <info4assistance25@gmail.com>
2026-07-21 15:13:21 +0200

    Delete src/lib/api.ts

--- src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-export function apiFetch(input: RequestInfo | URL, init: RequestInit = {}) {
-  const token = localStorage.getItem('sessionToken');
-  const headers = new Headers(init.headers || {});
-  if (token) {
-    headers.set('Authorization', `Bearer ${token}`);
-  }
-  return fetch(input, { ...init, headers });
-}
